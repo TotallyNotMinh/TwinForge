7129f68908bf03844e4a573bce717b51bd56ea17
TotallyNotMinh <nhatminh28072000@gmail.com>
2026-08-29 22:36:11 +0700

    Add conda env,RestNet encoder and  multi-head decoder

---
+++ models/encoder.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+from torchvision.models import resnet34, ResNet34_Weights
+
+
+class ResNetEncoder(nn.Module):
+    def __init__(self, pretrained=True, freeze=True):
+        super().__init__()
+
+        weights = ResNet34_Weights.DEFAULT if pretrained else None
+        resnet = resnet34(weights=weights)
+
+        self.stem = nn.Sequential(
+            resnet.conv1,
+            resnet.bn1,
+            resnet.relu,
+        )
+
+        self.maxpool = resnet.maxpool
+
+        self.layer1 = resnet.layer1 
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
+
+        if freeze:
+            for param in self.parameters():
+                param.requires_grad = False
+
+    def forward(self, x):
+        features = {}
+
+        x = self.stem(x)
+        features["f1"] = x # 64 channels
+
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        features["f2"] = x # 64 channels
+
+        x = self.layer2(x)
+        features["f3"] = x # 128 channels
+
+        x = self.layer3(x)
+        features["f4"] = x # 256 channels
+
+        x = self.layer4(x)
+        features["f5"] = x # 512 channels
+
+        return features
---
+++ models/multihead_decoder.py
@@ -0,0 +1,140 @@
+from torch import nn
+from torch import cat
+import torch
+from encoder import ResNetEncoder
+
+class SegmentHead(nn.Module):
+    def __init__(self, num_labels):
+        super().__init__()
+
+        self.out = nn.Conv2d(64, num_labels, kernel_size=3, padding=1)
+        
+    def forward(self, x):
+        return self.out(x)
+
+
+class DepthHead(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.out = nn.Conv2d(64, 1, kernel_size=3, padding=1)
+        
+    def forward(self, x):
+        return self.out(x)
+
+
+class BoundaryHead(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.out = nn.Conv2d(64, 1, kernel_size=3, padding=1)
+        
+    def forward(self, x):
+        return self.out(x)
+
+
+class MultiHeadDecoder(nn.Module):
+    def __init__(self, num_labels):
+        super().__init__()
+
+        self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
+
+        self.bot = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=3, padding=1), 
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+
+        self.dec1 = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+
+        self.dec2 = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
+        self.dec3 = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
+        self.dec4 = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
+        self.depth_head = DepthHead()
+        self.boundary_head = BoundaryHead()
+        self.segment_head = SegmentHead(num_labels)
+
+    def forward(self, encoded_features):
+        bottleneck = encoded_features["f5"]
+
+        up1 = self.bot(bottleneck)        
+        up1 = self.up(up1)
+
+        up2 = self.dec1(cat([up1, encoded_features["f4"]], dim=1))
+        up2 = self.up(up2)
+
+        up3 = self.dec2(cat([up2, encoded_features["f3"]], dim=1))
+        up3 = self.up(up3)
+
+        up4 = self.dec3(cat([up3, encoded_features["f2"]], dim=1))
+        up4 = self.up(up4)
+
+        up5 = self.dec4(cat([up4, encoded_features["f1"]], dim=1))
+        up5 = self.up(up5)
+
+        segment_logits = self.segment_head(up5)
+        depth_logits = self.depth_head(up5)
+        edge_logits = self.boundary_head(up5)
+
+        return segment_logits, depth_logits, edge_logits
+
+
+if __name__ == "__main__":
+    encoder = ResNetEncoder()
+    model = MultiHeadDecoder(num_labels=40)
+    image = torch.randn((1, 3, 160, 320))
+
+    features = encoder.forward(image)
+    segment, depth, edge = model.forward(features)
+    print(f"Output shape: Segment:{segment.shape}, Edge: {depth.shape}, Depth: {depth.shape}")
