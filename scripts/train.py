6e436245baf6b74eb021924016fdb998359ed3e1
TotallyNotMinh <nhatminh28072000@gmail.com>
2026-09-11 18:16:17 +0700

    Freeze first layers

--- scripts/train.py
+++ scripts/train.py
@@ -207,7 +207,7 @@ def train():
 
     # ============== Model ==============
     
-    model = TwinForge(NUM_CLASSES, NUM_HEADS, tok_dim=TOKEN_DIM, size=resize, freeze=False).to(device)
+    model = TwinForge(NUM_CLASSES, NUM_HEADS, tok_dim=TOKEN_DIM, size=resize, freeze_early=True).to(device)
 
     # ============== Optimizer and Schedulers ==============
     
